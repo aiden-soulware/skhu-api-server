19fe7d367d40cee1d46763fe315de15c94510a83
jhj960918 <jhj960918@gmail.com>
2021-02-17 14:09:26 +0900

    jun : detail

--- src/main/groovy/kr/soulware/skhuapi/service/JunMemberService.groovy
+++ src/main/groovy/kr/soulware/skhuapi/service/JunMemberService.groovy
@@ -1,23 +1,37 @@
 package kr.soulware.skhuapi.service
 
-
 import grails.gorm.transactions.Transactional
 import groovy.util.logging.Slf4j
 import kr.soulware.skhuapi.domain.Member
 import org.springframework.stereotype.Service
 
 @Slf4j
-@Transactional//create error
+@Transactional
+//create error
 @Service
 class JunMemberService {
 
-    List getList(Map params) {
+    List getList() {
 
         Member.findAll() as List
     }
-    List getListDeatail(Map params) {
 
-        Member.findById() as List
+    Member getListDeatail(long id) {
+
+        Member.get(id) as Member
+
+
     }
+    Member SaveListPost(Member member){
+        Member createObj = new Member(member)
+        createObj.save()
+    }
+
+    Member deleteById(long id) {
+        def deleteObj = Member.get(id)
+
+        deleteObj.delete()
+    }
+
 
 }
